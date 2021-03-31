366ce3b4fc1753dd7cb2792a8688ec9bba957026
Yoga Paripurna <63484583+YogaParipurna@users.noreply.github.com>
2021-03-31 11:19:47 +0700

    Add New Menu Pendaftaran + Activity Pendaftaran (Activity 4)

--- app/src/main/java/com/example/yah/MainActivity.java
+++ app/src/main/java/com/example/yah/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.yah;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +18,22 @@ public class MainActivity extends AppCompatActivity {
 
     String nama, password;
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.mnDaftar)
+        {
+            Intent i = new Intent(getApplicationContext(),ActivityDaftar.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
 
             public void onClick(View view) {
+
                 nama = edemail.getText().toString();
 
                 password = edpasswords.getText().toString();
